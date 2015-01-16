9c4e2cb3930fe8c3216d07d95c567beeed8ded45
Dinis Cruz <dinis.cruz@owasp.org>
2015-01-16 00:32:44 +0000

    Adding NWR support to travis and

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ var Server = require('./src/Server');           // gets the express server
 
 var server = new Server().start()               // start server
 
+function add_Swagger(app)
+  {
+    var Swagger_Service = require('./src/swagger/Swagger-Service')
+    var options = { app: app }
+    var swaggerService = new Swagger_Service(options)
+    swaggerService.set_Defaults()
+  }
+
+console.log('Adding swagger support')
+add_Swagger(server.app);
+
 console.log('Server started at: ' + server.url());
